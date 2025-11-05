77b55357c6c9997613dc10d540f525c60bf1ce03
ismail baoud <ibaoud46@egmail.com>
2025-11-05 19:19:36 +0100

    refactor: Amélioration du modèle et repository Fournisseur - Optimisation de l'entité Fournisseur - Ajout de méthodes de recherche dans FournisseurRepository

--- Gestion-des-Fournisseurs-spring-core/src/main/java/org/ismail/Tricol/repository/FournisseurRepository.java
+++ Gestion-des-Fournisseurs-spring-core/src/main/java/org/ismail/Tricol/repository/FournisseurRepository.java
@@ -3,11 +3,13 @@ package org.ismail.Tricol.repository;
 import org.ismail.Tricol.model.Fournisseur;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
+
 public interface FournisseurRepository extends JpaRepository<Fournisseur, Long> {
 
     @Query("select f from Fournisseur f where f.nom = :nom")
-    Fournisseur findByNom(String nom);
+    Fournisseur findByNom(@Param("nom") String nom);
 
 
     @Query("select f from Fournisseur f order by f.nom asc")

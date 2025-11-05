b47a0a68d6ed1072b0527ba9aca228c360776ab6
ismail baoud <ibaoud46@egmail.com>
2025-11-05 19:18:06 +0100

    feat: Amélioration des entités Commande et CommandeProduit - Ajout de la méthode calculateTotalPrice() utilisant la méthode CUMP (Coût Unitaire Moyen Pondéré) - Amélioration de la relation bidirectionnelle entre Commande et CommandeProduit - Ajout de validations et contraintes sur les entités - Optimisation de la gestion du montant total de la commande

--- gestion des commmendsFournisseur spring boot/src/main/java/org/ismail/gestiondescommmendsfournisseurspringboot/model/CommandeProduit.java
+++ gestion des commmendsFournisseur spring boot/src/main/java/org/ismail/gestiondescommmendsfournisseurspringboot/model/CommandeProduit.java
@@ -6,10 +6,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity(name = "commande_produit")
 @Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommandeProduit {
